610c93b7307127724b7bd1627992aac97f28a110
ndallich1 <ndallich@gmail.com>
2023-01-02 19:51:32 -0500

    Printed stored items to page

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ let saveButtonEl = $('.saveBtn');
 let scheduleEl = $('input[name="description"]');
 console.log(saveButtonEl);
 console.log(scheduleEl);
+$("#hour-8 .description").val(localStorage.getItem("hour-8"))
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
 
 $(function () {
   /* TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be
@@ -24,11 +33,11 @@ function displayColor() {
     }
      
     if (timeBlocks[i] == now) {
-    $(`#hour-${timeBlocks[i]}`).addClass('present'); 
+    $(`#hour-${timeBlocks[i]}`).addClass('present');  
     }
    
     if (timeBlocks[i] < now) {
-    $(`#hour-${timeBlocks[i]}`).addClass('past'); 
+    $(`#hour-${timeBlocks[i]}`).addClass('past');  
     }
     console.log($(`hour-${i}`));
   }
@@ -51,7 +60,6 @@ displayColor();
     localStorage.setItem(timeBlockId, scheduleText);
   }
   // Hint: difficult part = save under a keyname that will be recognized. Retrieve from local storage and write to a string.  
-
   
   /* TODO: Add code to display the current date in the header of the page. */
   // Complete
